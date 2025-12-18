9cf3443d7aeaaf80c911041c8c924e63d0fe9ee0
altheaiyah <altheamishkaalvarez@gmail.com>
2025-12-18 13:04:27 +0800

    Add dynamic content loading

---
+++ activities.js
@@ -0,0 +1,34 @@
+var activities = [
+  {
+    title: "AirPods Max",
+    subTitle: "Hardware Device Showcase",
+    image: "assets/images/img1.png",
+    viewLink: "https://altheaiyah.github.io/Apple-AirPods-Max/",
+    repoLink: "https://github.com/altheaiyah/Apple-AirPods-Max",
+    description: "A showcase of the Apple AirPods Max, emphasizing its innovative design, advanced features, and sleek aesthetics."
+  },
+  {
+    title: "VayanBatanes",
+    subTitle: "Place Showcase",
+    image: "assets/images/img2.png",
+    viewLink: "https://altheaiyah.github.io/Philippines-Batanes/",
+    repoLink: "https://github.com/altheaiyah/Philippines-Batanes",
+    description: "This project highlights Batanes, featuring the three main islands: Batan, Sabtang, and Itbayat, showcasing their scenic landscapes and Ivatan heritage."
+  },
+  {
+    title: "BTS",
+    subTitle: "Gallery using javascript",
+    image: "assets/images/img3.png",
+    viewLink: "https://altheaiyah.github.io/Gallery/",
+    repoLink: "https://github.com/altheaiyah/Gallery",
+    description: "This is a BTS gallery website showcasing member profiles and albums, incorporating interactive functionality to enhance user engagement."
+  },
+  {
+    title: "Nintendo Amiibo Series",
+    subTitle: "Activity using API",
+    image: "assets/images/img4.png",
+    viewLink: "https://altheaiyah.github.io/WD-API/",
+    repoLink: "https://github.com/altheaiyah/WD-API",
+    description: "An activity that utilizes an API to show its content."
+  }
+];
---
+++ certificates.js
@@ -0,0 +1,8 @@
+var certificates = [
+  {
+    title: "Certificate of Recognition - Networking Basics",
+    subTitle: "Cisco Networking Academy, 2025",
+    image: "assets/images/networking-basics-cert.jpg",
+    description: "Received a certificate for successfully completing the free online course Networking Basics."
+  }
+];
